fbad55800ef9b663dfd0762575d1c714a40782f4
Ivan Grimes <ivan@grimesco.de>
2018-10-02 12:15:15 +0300

    add additional css

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,9 +3,8 @@ import CalendarContainer from '../Calendar/CalendarContainer';
 import Events from '../Events';
 import css from './App.module.css';
 
+// TODO: Put reusable css classes into global.module.css
 // TODO: Edit propTypes
-// TODO: Write CSS
-// TODO: May be write testss
 
 const App = () => (
   <div className={css.container}>
